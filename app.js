4110b3a186b1172ce4bd4058b6cb711f7e3b1f6c
XavierAgostini <xavier.agostini@mail.mcgill.ca>
2021-03-15 20:46:00 -0700

    get options parsing working for all option types

--- app.js
+++ app.js
@@ -1,35 +1,42 @@
 // https://api.tdameritrade.com/v1/accounts/497245329/transactions
 const raw_transactions = require('./data/transactions2.json')
-
+const fs = require('fs')
 const transactions = raw_transactions.reverse()
-const optionIdMap = {}
-const thetaTrades = {}
-const optionTrades = {}
+
 
 const closeTrade = (option) => {
   let openDate = new Date(new Date(option.open.date).toDateString())
   let closeDate = new Date(new Date(option.close.date).toDateString())
   option.status = 'closed'
   option.results = {}
-  option.results['P/L'] = parseFloat((option.open.premium - option.close.cost).toFixed(2))
-  option.results['P/L%'] = parseFloat(((option.open.premium - option.close.cost)*100/option.open.premium).toFixed(2))
+
+  if (option.tradeType === 'short') {
+    option.results['P/L'] = parseFloat((option.open.cost - option.close.cost).toFixed(2))
+    option.results['P/L%'] = parseFloat(((option.open.cost - option.close.cost)*100/option.open.cost).toFixed(2))
+    if (option.type === 'PUT') {
+      option.results['ROR%'] = parseFloat((100*option.results['P/L']/option.open.collateral).toFixed(2))
+    }
+  } else {
+    option.results['P/L'] = parseFloat((option.close.cost - option.open.cost).toFixed(2))
+    option.results['P/L%'] = parseFloat(((option.close.cost - option.open.cost)*100/option.open.cost).toFixed(2))
+    option.results['ROR%'] = parseFloat((100*option.results['P/L']/option.open.cost).toFixed(2))
+  }
+  
   option.results['days_held'] = (closeDate.getTime() - openDate.getTime())/(1000 * 3600 * 24)
 
-  if (option.type === 'PUT') {
-    option.results['ROR%'] = parseFloat((100*option.results['P/L']/option.open.collateral).toFixed(2))
-  }
+  
 }
 
 
 const getOpenInfo = () => {
   let putPremium = 0
   let callPremium = 0
-  Object.keys(thetaTrades).forEach(trade => {
-    if (thetaTrades[trade].type === 'PUT' && thetaTrades[trade].status === 'open') {
-      putPremium += thetaTrades[trade].open.premium
+  Object.keys(options).forEach(trade => {
+    if (options[trade].type === 'PUT' && options[trade].status === 'open') {
+      putPremium += options[trade].open.cost
     }
-    else if (thetaTrades[trade].type === 'CALL' && thetaTrades[trade].status === 'open') {
-      callPremium += thetaTrades[trade].open.premium
+    else if (options[trade].type === 'CALL' && options[trade].status === 'open') {
+      callPremium += options[trade].open.cost
     }
   })
   return {
@@ -38,20 +45,20 @@ const getOpenInfo = () => {
     total: putPremium + callPremium
   }
 }
-const analyzeThetaTrades = () => {
+const analyzeoptions = () => {
   let closedPremium = 0
   let numClosed = 0
   let numOpen = 0
   let openPremium = 0
   let totCollateral = 0
-  Object.values(thetaTrades).forEach(trade => {
+  Object.values(options).forEach(trade => {
     if (trade.status === 'closed') {
-      console.log(trade.results)
-      closedPremium += trade.open.premium - trade.close.cost
+      // console.log(trade.results)
+      closedPremium += trade.open.cost - trade.close.cost
       numClosed += 1
     } else {
       totCollateral += trade.open.collateral
-      openPremium += trade.open.premium
+      openPremium += trade.open.cost
       numOpen += 1
     }
   })
@@ -59,97 +66,154 @@ const analyzeThetaTrades = () => {
 
 }
 
-transactions.map(transaction => {
-  // if transaction related to an option trade
-  if (transaction.type === 'TRADE' && transaction.transactionItem.instrument && transaction.transactionItem.instrument.assetType === 'OPTION') {
-    let option = transaction.transactionItem
-    let optionName = option.instrument.description
+const parseTransactions = (transactions) => {
+  const optionIdMap = {}
+  const options = {}
 
-    const descriptionArr = optionName.split(' ')
-    let underlyingSymbol = option.instrument.underlyingSymbol
-    let optionType = option.instrument.putCall
-    let strike = descriptionArr[descriptionArr.length - 2]
-    let exp_date = option.instrument.optionExpirationDate
-    let cusip = option.instrument.cusip
+  transactions.map(transaction => {
+    // if transaction related to an option trade
+    if (transaction.type === 'TRADE' && transaction.transactionItem.instrument && transaction.transactionItem.instrument.assetType === 'OPTION') {
+      let option = transaction.transactionItem
+      let optionName = option.instrument.description
 
-    // console.log(option)
-    if (option.instruction === 'SELL' && option.positionEffect === 'OPENING') {  
-      if (!optionIdMap[cusip]) optionIdMap[cusip] = optionName
+      const descriptionArr = optionName.split(' ')
+      let underlyingSymbol = option.instrument.underlyingSymbol
+      let optionType = option.instrument.putCall
+      let strike = descriptionArr[descriptionArr.length - 2]
+      let exp_date = option.instrument.optionExpirationDate
+      let cusip = option.instrument.cusip
 
-      if(!thetaTrades[optionName]) {
-        thetaTrades[optionName] = {
-          stock_symbol: underlyingSymbol,
-          exp_date,
-          type: optionType,
-          strike: strike,
-          status: 'open',
-          open: {
-            date: transaction.transactionDate,
-            quantity: 0,
-            price: 0,
-            premium: 0,
-            collateral: 0
-          },
-          close: {
-            quantity: 0,
-            price: 0,
-            cost: 0
+      const optionSymbol = option.instrument.symbol
+      // console.log(option)
+      if (option.instruction === 'SELL' && option.positionEffect === 'OPENING') {  
+        if (!optionIdMap[cusip]) optionIdMap[cusip] = optionSymbol
+
+        if(!options[optionSymbol]) {
+          options[optionSymbol] = {
+            id: optionSymbol,
+            name: optionName,
+            stock_symbol: underlyingSymbol,
+            exp_date,
+            type: optionType,
+            tradeType: 'short',
+            strike: strike,
+            status: 'open',
+            open: {
+              date: transaction.transactionDate,
+              quantity: 0,
+              price: 0,
+              cost: 0,
+              collateral: 0
+            },
+            close: {
+              quantity: 0,
+              price: 0,
+              cost: 0
+            }
           }
         }
+        let openLeg = options[optionSymbol].open
+        openLeg.price = (option.price *  option.amount + openLeg.price * openLeg.quantity) / (option.amount + openLeg.quantity)
+        openLeg.quantity += option.amount
+        openLeg.cost += transaction.netAmount
+        if (optionType === 'PUT') openLeg.collateral += option.amount * strike * 100
       }
-      let openLeg = thetaTrades[optionName].open
-      openLeg.price = (option.price *  option.amount + openLeg.price * openLeg.quantity) / (option.amount + openLeg.quantity)
-      openLeg.quantity += option.amount
-      openLeg.premium += transaction.netAmount
-      if (optionType === 'PUT') openLeg.collateral += option.amount * strike * 100
-    }
-    else if (option.instruction === 'BUY' && option.positionEffect === 'CLOSING') {
-      let closeLeg = thetaTrades[optionName].close
-      closeLeg.price = (option.price *  option.amount + closeLeg.price * closeLeg.quantity) / (option.amount + closeLeg.quantity)
-      closeLeg.quantity += option.amount
-      closeLeg.cost -= transaction.netAmount
-      closeLeg.date = transaction.transactionDate
-      if (thetaTrades[optionName].open.quantity === closeLeg.quantity) {
-        closeTrade(thetaTrades[optionName])
+      else if (option.instruction === 'BUY' && option.positionEffect === 'CLOSING') {
+        let closeLeg = options[optionSymbol].close
+        closeLeg.price = (option.price *  option.amount + closeLeg.price * closeLeg.quantity) / (option.amount + closeLeg.quantity)
+        closeLeg.quantity += option.amount
+        closeLeg.cost -= transaction.netAmount
+        closeLeg.date = transaction.transactionDate
+        if (options[optionSymbol].open.quantity === closeLeg.quantity) {
+          closeTrade(options[optionSymbol])
+        }
       }
-    }
-    // else if (option.instruction === 'BUY') {
-    //   if (!optionTrades[optionName]) {
-    //     optionTrades[optionName] = {
-    //     stock_symbol: underlyingSymbol,
-    //     exp_date,
-    //     type: optionType,
-    //     strike: strike,
-    //     status: 'open',
-    //     open: {
-    //       date: transaction.transactionDate,
-    //       quantity: 0,
-    //       price: 0,
-    //       premium: 0,
-    //       collateral: 0
-    //     },
-    //     close: {
-    //       quantity: 0,
-    //       price: 0,
-    //       cost: 0
-    //     }
-    //   }}
-    // }
-  }
-  // if transaction related to option expirary
-  else if (transaction.transactionSubType === 'OX') {
-    let cusip = transaction.transactionItem.instrument.cusip
-    if (optionIdMap[cusip]) {
-      let option = thetaTrades[optionIdMap[cusip]]
-      let closeLeg = option.close
-      closeLeg.quantity += transaction.transactionItem.amount
-      closeLeg.date = transaction.transactionDate
+      else if (option.instruction === 'BUY' && option.positionEffect === 'OPENING') {
+        if (!optionIdMap[cusip]) optionIdMap[cusip] = optionSymbol
 
-      if (option.open.quantity === closeLeg.quantity) {
-        closeTrade(thetaTrades[optionIdMap[cusip]])
+        if(!options[optionSymbol]) {
+          options[optionSymbol] = {
+            id: optionSymbol,
+            name: optionName,
+            stock_symbol: underlyingSymbol,
+            exp_date,
+            type: optionType,
+            tradeType: 'long',
+            strike: strike,
+            status: 'open',
+            open: {
+              date: transaction.transactionDate,
+              quantity: 0,
+              price: 0,
+              cost: 0
+            },
+            close: {
+              quantity: 0,
+              price: 0,
+              cost: 0
+            }
+          }
+        }
+        let openLeg = options[optionSymbol].open
+        openLeg.price = (option.price *  option.amount + openLeg.price * openLeg.quantity) / (option.amount + openLeg.quantity)
+        openLeg.quantity += option.amount
+        openLeg.cost -= transaction.netAmount
+      }
+      else if (option.instruction === 'SELL' && option.positionEffect === 'CLOSING' && options[optionSymbol]) {
+        
+        let closeLeg = options[optionSymbol].close
+        closeLeg.price = (option.price *  option.amount + closeLeg.price * closeLeg.quantity) / (option.amount + closeLeg.quantity)
+        closeLeg.quantity += option.amount
+        closeLeg.cost += transaction.netAmount
+        closeLeg.date = transaction.transactionDate
+        if (options[optionSymbol].open.quantity === closeLeg.quantity) {
+          closeTrade(options[optionSymbol])
+        }
       }
     }
-  }
-})
-analyzeThetaTrades()
-console.log('getOpenInfo', getOpenInfo())
+    // if transaction related to option expirary
+    else if (transaction.transactionSubType === 'OX') {
+      let cusip = transaction.transactionItem.instrument.cusip
+      if (optionIdMap[cusip]) {
+        let option = options[optionIdMap[cusip]] || options[optionIdMap[cusip]]
+        // console.log(optionIdMap[cusip])
+        let closeLeg = option.close
+        closeLeg.quantity += transaction.transactionItem.amount
+        closeLeg.date = transaction.transactionDate
+
+        if (option.open.quantity === closeLeg.quantity) {
+          closeTrade(option)
+        }
+      }
+    }
+    else if (transaction.transactionSubType === 'OE') {
+      let cusip = transaction.transactionItem.instrument.cusip
+      if (optionIdMap[cusip]) {
+        let option = options[optionIdMap[cusip]] || options[optionIdMap[cusip]]
+        // console.log(optionIdMap[cusip])
+        let closeLeg = option.close
+        closeLeg.quantity += transaction.transactionItem.amount
+        closeLeg.date = transaction.transactionDate
+        closeLeg.cost = closeLeg.quantity * option.strike
+        closeLeg['note'] = 'option exercised'
+        if (option.open.quantity === closeLeg.quantity) {
+          closeTrade(option)
+        }
+      }
+    }
+  })
+  return options
+}
+
+// analyzeoptions()
+// console.log('getOpenInfo', getOpenInfo())
+const saveTradesToJSON = (transactions) => {
+  console.log("Parsing transactions...")
+  const optionTrades = parseTransactions(transactions)
+  console.log(optionTrades)
+  console.log("Writing options trades to file")
+  fs.writeFileSync('./data/optionTrades.json', JSON.stringify(Object.values(optionTrades)))
+  console.log("Done writting!")
+}
+
+saveTradesToJSON(transactions)
--- src/components/ThetaTable/index.tsx
+++ src/components/ThetaTable/index.tsx
@@ -19,57 +19,16 @@ import Search from '@material-ui/icons/Search';
 import ViewColumn from '@material-ui/icons/ViewColumn';
 import { SvgIconProps } from '@material-ui/core';
 
+import data from './optionTrades.json'
+
 const ThetaTable = () => {
   const columns = [
-    { title: 'Title', field: 'title' },
-    { title: 'Author', field: 'authors' },
-    { title: 'Page Count', field: 'num_pages' },
-    { title: 'Rating', field: 'rating' }
+    { title: 'Name', field: 'name' },
+    { title: 'Stock', field: 'stock_symbol' },
+    { title: 'Exp. Date', field: 'exp_date' },
+    { title: 'Type', field: 'type' }
   ]
-  const data = [
-    {
-      id: 1,
-      title: 'The Hunger Games',
-      authors: 'Suzanne Collins',
-      num_pages: 374,
-      rating: 4.33
-    },
-    {
-      id: 2,
-      title: 'Harry Potter and the Order of the Phoenix',
-      authors: 'J.K. Rowling',
-      num_pages: 870,
-      rating: 4.48
-    },
-    {
-      id: 3,
-      title: 'To Kill a Mockingbird',
-      authors: 'Harper Lee',
-      num_pages: 324,
-      rating: 4.27
-    },
-    {
-      id: 4,
-      title: 'Pride and Prejudice',
-      authors: 'Jane Austen',
-      num_pages: 279,
-      rating: 4.25
-    },
-    {
-      id: 5,
-      title: 'Twilight',
-      authors: 'Stephenie Meyer',
-      num_pages: 498,
-      rating: 3.58
-    },
-    {
-      id: 6,
-      title: 'The Book Thief',
-      authors: 'Markus Zusak',
-      num_pages: 552,
-      rating: 4.36
-    }
-  ]
+  
   const tableIcons = {
     Add: forwardRef<SVGSVGElement, SvgIconProps>((props, ref) => (
       <AddBox {...props} ref={ref} />
